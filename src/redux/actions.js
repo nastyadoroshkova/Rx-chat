1bde57ce8e446c1b2ae89a0c64eccd20f5c916ed
nastyadoroshkova <doroskova.nastya@gmail.com>
2021-10-29 16:17:00 +0300

    changed websocket url

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -28,7 +28,7 @@ const configClient = () => {
       metadataMimeType: MESSAGE_RSOCKET_ROUTING.string
     },
     transport: new RSocketWebSocketClient({
-      url: "wss://green-chat-33jco.ondigitalocean.app",
+      url: "wss://green-chat-hpthl.ondigitalocean.app",
     }),
   });
 };
